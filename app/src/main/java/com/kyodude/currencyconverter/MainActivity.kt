2a28f3ca819814b2fd6feea73463b39a5bedaca5
Kunal Yelne <kunal17058@gmail.com>
2021-01-27 09:34:23 +0530

    Finishes HomeActivity with necessary data

--- app/src/main/java/com/kyodude/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/kyodude/currencyconverter/MainActivity.kt
@@ -1,15 +1,53 @@
 package com.kyodude.currencyconverter
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.kyodude.currencyconverter.databinding.ActivityMainBinding
+import com.kyodude.currencyconverter.main.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            viewModel.convert(
+                binding.etFrom.text.toString(),
+                binding.spFromCurrency.selectedItem.toString(),
+                binding.spToCurrency.selectedItem.toString()
+            )
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.conversion.collect { event ->
+                when (event) {
+                    is MainViewModel.CurrencyEvent.Success -> {
+                        binding.progressBar.isVisible = false
+                        binding.result.setTextColor(Color.BLACK)
+                        binding.result.text = event.resultText
+                    }
+                    is MainViewModel.CurrencyEvent.Failure -> {
+                        binding.progressBar.isVisible = false
+                        binding.result.setTextColor(Color.RED)
+                        binding.result.text = event.errorText
+                    }
+                    is MainViewModel.CurrencyEvent.Loading -> {
+                        binding.progressBar.isVisible = true
+                    }
+                    else -> Unit
+                }
+            }
+        }
     }
 }
